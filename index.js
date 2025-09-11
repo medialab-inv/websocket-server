c8b7f7ebb0d432ced0fbd136dcc803d314d6a0d2
rodrigflorencia <rodrig.florencia@gmail.com>
2025-09-10 22:22:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ wss.on('connection', (ws) => {
             
             // Manejar solicitud del último mensaje
             if (data.action === 'load' && data.fileName) {
-               
+                 console.log('Enviado:', message.toString());
                 return ws.send(JSON.stringify(lastMessage));
             }
             
